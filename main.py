7a31c2e36fccca6a6dcd948eb0bc4bbc05ae5ba4
Zekicks <dimagamerakk@gmail.com>
2022-05-23 16:29:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
+import logging
 
-BOT_TOKEN = "5329981275:AAEaqp95hFPnVVVR6UMPayX2c6G8xFJ1KG0"
+from aiogram import Bot, Dispatcher, executor, types
 
-async def start_handler(event: types.Message):
-    await event.answer(
-        f"Hello, {event.from_user.get_mention(as_html=True)} 👋!",
-        parse_mode=types.ParseMode.HTML,
+
+API_TOKEN = ''
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def start_cmd_handler(message: types.Message):
+    keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
+
+
+    text_and_data = (
+        ('Rus 🇷🇺', 'rus'),
+        ('Eng 🇬🇧', 'eng'),
+    )
+    row_btns = (types.InlineKeyboardButton(text, callback_data=data) for text, data in text_and_data)
+
+    keyboard_markup.row(*row_btns)
+    keyboard_markup.add(
+
+        types.InlineKeyboardButton('developer', url='https://t.me/zekicks'),
     )
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    try:
-        disp = Dispatcher(bot=bot)
-        disp.register_message_handler(start_handler, commands={"start", "restart"})
-        await disp.start_polling()
-    finally:
-        await bot.close()
+    await message.reply("Привет, выбери язык!\nHello, choose a language!", reply_markup=keyboard_markup)
 
-asyncio.run(main())
+
+@dp.callback_query_handler(text='rus')  
+@dp.callback_query_handler(text='eng')  
+async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
+    answer_data = query.data
+
+    await query.answer(f'You answered with {answer_data!r}')
+
+    if answer_data == 'rus':
+        text = 'Выбран русский язык!'
+    elif answer_data == 'eng':
+        text = 'selected English'
+
+    await bot.send_message(query.from_user.id, text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
